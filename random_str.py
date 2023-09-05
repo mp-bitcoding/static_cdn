d050634aa72f0f329cdf37b667612a96e14534fc
never-known-soldier <dhingravassu2915@gmail.com>
2023-09-05 14:39:31 +0530

    create file for get random string from list

---
+++ random_str.py
@@ -0,0 +1,9 @@
+import random
+
+# List of strings
+string_list = ["apple", "banana", "cherry", "date", "elderberry"]
+
+# Get a random string from the list
+random_string = random.choice(string_list)
+
+print(random_string)
